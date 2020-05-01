1f2f3d7be4319acb2125a27d75c7e37c55c4116c
Gaurav Agarwal <gauravggg21@gmail.com>
2020-05-02 03:21:27 +0530

    final

---
+++ src/lambda/http/getAll.ts
@@ -0,0 +1,27 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const ridesTable = process.env.RIDES_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event)
+
+  const result = await docClient.scan({
+    TableName: ridesTable
+  }).promise()
+
+  const items = result.Items
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items
+    })
+  }
+}
---
+++ src/lambda/http/getCancelled.ts
@@ -0,0 +1,30 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const ridesTable = process.env.RIDES_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event)
+
+  const result = await docClient.scan({
+    TableName: ridesTable,
+    ProjectionExpression: 'from_city_id, from_date, Car_Cancellation',
+    FilterExpression: 'Car_Cancellation = :one',
+    ExpressionAttributeValues : {':one' : 1}
+  }).promise()
+
+  const items = result.Items
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items
+    })
+  }
+}
